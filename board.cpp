4a687636cd46c8b7afbc37f53eb7bfafa4d9a912
Ben Cordes <cordes.ben@gmail.com>
2015-09-24 23:12:03 -0400

    Make the brute-force solver quiet; add comments

--- board.cpp
+++ board.cpp
@@ -3,6 +3,8 @@
 
 #include "board.h"
 
+#define USE_HEURISTICS
+
 Board::Board() {
   this->board = 0;
   this->height = 0;
@@ -99,6 +101,7 @@ void showint(int i) {
 }
 
 bool Board::brute_force(Board::Line &line) {
+  // Count the number of each color in the line.
   int blacks=0, whites=0, nones=0;
   for (Board::iterator it = line.begin; it < line.end; it++) {
     switch(it->get()) {
@@ -110,29 +113,43 @@ bool Board::brute_force(Board::Line &line) {
         whites++; break;
     }
   }
+  // If the line is complete, why are we here?
   if (nones == 0) { return false; }
 
   bool changes = false;
   int i=0;
   int count = line.count();
+  // The only reason this is a vector<int> and not vector<Color> is for easier
+  // printing. This is almost certainly fixable.
+  std::vector< int > perm;
 
-  std::cout << "Baseline: ";
-  std::cout << line.to_str() << "\n";
+  //std::cout << "Baseline: ";
+  //std::cout << line.to_str() << "\n";
 
+  // Temporary line, holding a copy of the current line plus the permuted
+  // possible additional values
   Square *tempboard = new Square[count];
 
+  // Temporary line, holding the reduction of all valid permutations
   Square *results = new Square[count];
   for (i=0; i<count; i++) { results[i].set(Square::NONE); }
 
+  // Iterators over tempboard
   Board::iterator tempstart(tempboard, 1);
   Board::iterator tempend(tempboard + count, 1);
   Board::Line templine(tempstart, tempend);
 
-  std::vector< int > perm;
+  // Determine how many white and black squares are left to be added to this
+  // line (game rule: exactly half of the line is each color)
   for (i=0; i<(count / 2) - whites; i++) { perm.push_back(Square::WHITE); }
   for (i=0; i<(count / 2) - blacks; i++) { perm.push_back(Square::BLACK); }
+
+  // Permutation loop.
   do {
     i = 0;
+    // Write {templine} with (line with empty tiles) + (permutation of tiles).
+    // In other words, for every NONE square in the current line, grab a tile
+    // off the permutation and drop it in place.
     std::vector< int >::iterator perm_ptr = perm.begin();
     for (Board::iterator line_ptr = line.begin; line_ptr != line.end; line_ptr++) {
       Square::Color col = line_ptr->get();
@@ -144,19 +161,26 @@ bool Board::brute_force(Board::Line &line) {
       }
       i++;
     }
-    std::cout << "Applied:  " << templine.to_str() << "  perm: ";
-    std::for_each(perm.begin(), perm.end(), &showint);
+    //std::cout << "Applied:  " << templine.to_str() << "  perm: ";
+    //std::for_each(perm.begin(), perm.end(), &showint);
 
+    /* If the applied combination of tiles is valid (has no triplets), then try
+     * to "save" its value onto the {results} line. This is a reduce()
+     * operation; the goal is to say, if every valid permutation has a WHITE in
+     * index 6, then index 6 *must* be WHITE. But if one valid perm has WHITE
+     * and another valid perm has BLACK, then we don't know what index 6 should
+     * be, so don't write anything. */
     if (templine.is_valid()) {
-      std::cout << " valid";
+      //std::cout << " valid";
       for (i=0; i<count; i++) {
         Square::Color res, tmp;
         res = results[i].get();
         tmp = tempboard[i].get();
-        /* If the results square is NONE, then write the current value.
+        /* If the results square is NONE, then write the temporary value.
          * If the results square is DEAD, then leave it DEAD.
-         * If the results square is different than the current value, mark it DEAD.
-         * If the results square is the same, we're in good shape (leave it as is). */
+         * If the results square is different than the temporary value, mark
+         *   the results square DEAD.
+         * If the results square is the same, leave it as it is. */
         if (res == Square::NONE) 
           results[i].set(tmp);
         else if (res != tmp)
@@ -164,15 +188,20 @@ bool Board::brute_force(Board::Line &line) {
       }
     }
 
-    std::cout << "\n";
+    //std::cout << "\n";
 
+  // This is the magic permutation function (thanks, STL!)
   } while(std::next_permutation(perm.begin(), perm.end()));
 
+  /*
   Board::iterator resstart(results, 1);
   Board::iterator resend(results + count, 1);
   Board::Line resline(resstart, resend);
   std::cout << "Changes:  " << resline.to_str();
+  */
 
+  // Iterate over the {results} line. Any square that (a) isn't marked DEAD (b)
+  // isn't already present in {line} should be updated with the {result}.
   i = 0;
   for (Board::iterator line_ptr = line.begin; line_ptr != line.end; line_ptr++) {
     Square::Color res = results[i].get();
@@ -183,11 +212,16 @@ bool Board::brute_force(Board::Line &line) {
     i++;
   }
 
+  /*
   if (! changes) {
     std::cout << "  none applied";
   }
   std::cout << "\n";
+  */
 
+  // Clean up after yourself and report whether you did anything.
+  delete(tempboard);
+  delete(results);
   return changes;
 }
 
@@ -195,7 +229,11 @@ bool Board::brute_force(Board::Line &line) {
  * everything below a certain threshold, then try to brute-force each line. As
  * soon as one is successful, stop. */
 bool Board::solve_by_brute_force() {
+#ifdef USE_HEURISTICS
+  int cutoff = 3;
+#else
   int cutoff = 1;
+#endif
   std::vector< Board::Line > lines;
   for (int x = 0; x < width; x++) {
     Board::Line col = fetch_col(x);
@@ -291,7 +329,7 @@ bool Board::solve_by_finder(Board::FindFunc ff) {
 void Board::solve() {
   bool changes;
   while (!is_solved()) {
-    /*
+#ifdef USE_HEURISTICS
     changes = true;
     while (changes) {
       changes = false;
@@ -300,10 +338,11 @@ void Board::solve() {
       changes = changes || solve_by_finder(&Board::find_halves);
       //if (changes) std::cout << to_str();
     }
-    */
+#endif
     changes = solve_by_brute_force();
     // Either we're done, or something went really really wrong.
     if (!changes) { return; }
-    std::cout << to_str();
+    //std::cout << to_str();
   }
+  std::cout << to_str();
 }
